861f80d255c6b19e775ab45209f743eb8c428253
Mateusz Nurzyński <mateonurzynski@gmail.com>
2022-07-04 18:21:36 +0200

    Create screen typing function

--- main.js
+++ main.js
@@ -1,3 +1,42 @@
+const digits = document.querySelectorAll('.digit');
+const screen = document.querySelector('.screen');
+digits.forEach((digit) => {
+  digit.addEventListener('click', addNumber);
+});
+
+let screenContent = '0';
+updateScreen();
+function addNumber(e) {
+  const SCREEN_LIMIT = 12;
+  if (screenContent.length >= SCREEN_LIMIT) {
+    return;
+  }
+  const number =
+    e.currentTarget.textContent !== ','
+      ? +e.currentTarget.textContent
+      : e.currentTarget.textContent;
+  if (number === 0 && screenContent === '0') {
+    return;
+  }
+  if (screenContent === '0') {
+    if (number !== ',') {
+      screenContent = '';
+    }
+    screenContent += number;
+    updateScreen();
+    return;
+  }
+  screenContent += number;
+  updateScreen();
+}
+
+function updateScreen(forcedValue) {
+  if (forcedValue) {
+    screenContent = forcedValue;
+  }
+  screen.textContent = screenContent;
+}
+
 function add(num1, num2) {
   return +num1 + +num2;
 }
