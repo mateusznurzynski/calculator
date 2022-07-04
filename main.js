829f774034dd00a7991cfaec29dbb16b0f4f4509
Mateusz Nurzyński <mateonurzynski@gmail.com>
2022-07-04 17:53:37 +0200

    Create a function that performs calculations

--- main.js
+++ main.js
@@ -10,3 +10,22 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
   return +num1 / +num2;
 }
+
+function operate(num1, num2, operator) {
+  let result = 0;
+  switch (operator) {
+    case '+':
+      result = add(num1, num2);
+      break;
+    case '-':
+      result = subtract(num1, num2);
+      break;
+    case '*':
+      result = multiply(num1, num2);
+      break;
+    case '/':
+      result = divide(num1, num2);
+      break;
+  }
+  return result;
+}
